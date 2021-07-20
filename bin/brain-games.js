46de02479ec7217e93c96a3873c28312bed14eb6
DimaKichigin <dimakichigin94@gmail.com>
2021-07-20 12:16:27 +0300

    -a

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
-const name = ('Welcome to the Brain Games!')
-console.log(name)
+// const nameOfGame = ('Welcome to the Brain Games!');
+//console.log('Welcome to the Brain Games!')
+import { name } from '../src/cli.js';
+console.log(`Hello, ${name}!`); 
+
+
