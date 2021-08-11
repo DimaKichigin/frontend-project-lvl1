477435840ef1aa7a62754e37a5e70455dc0be7b2
DimaKichigin <dimakichigin94@gmail.com>
2021-08-11 10:44:25 +0300

    add bin/brain-calc.js and games/calc.js and renamed generateRandomNumber.js -> utils.js

---
+++ src/games/calc.js
@@ -0,0 +1,33 @@
+import randomNum from '../utils.js';
+import engine from '../game-engine.js';
+
+const calculate = (num1, num2, sign) => {
+  let correctAnswer;
+  switch (sign) {
+    case '+': correctAnswer = num1 + num2;
+      break;
+    case '-': correctAnswer = num1 - num2;
+      break;
+    case '*': correctAnswer = num1 * num2;
+      break;
+    default: return null;
+  }
+  return correctAnswer;
+};
+const getGameData = () => {
+  const signs = ['+', '-', '*'];
+  const indexOfLastSign = signs.length - 1;
+  const num1 = randomNum(1, 50);
+  const num2 = randomNum(1, 50);
+  const sign = signs[(randomNum(0, indexOfLastSign))];
+  const question = `${num1} ${sign} ${num2}`;
+  const result = calculate(num1, num2, sign);
+  return [question, String(result)];
+};
+const rule = 'What is the result of the expression?';
+
+const startCalcGame = () => {
+  engine(rule, getGameData);
+};
+
+export default () => startCalcGame();
--- src/games/even.js
+++ src/games/even.js
@@ -1,35 +1,17 @@
-/* import generateRandomNumber from '../generateRandomNumber.js';
-import runGame from '../game-engine.js';
+import randomNum from '../utils.js';
+import engine from '../game-engine.js';
 
 // Определение четности числа
 const isEven = (num) => num % 2 === 0;
 
-// Задание
-const description = 'Answer "yes" if the number is even. Otherwise answer "no".';
-
 // Формирование пар "Вопрос-ответ"
-const generateGameConditions = () => {
-  const question = generateRandomNumber();
-  const answer = isEven(question) ? 'yes' : 'no';
-  return [`${question}`, answer];
-};
-const exportFunctions = () => {
-  runGame(description, generateGameConditions);
-};
-
-export default exportFunctions; */
-
-import randomNum from '../generateRandomNumber.js';
-import engine from '../game-engine.js';
-
-const isEven = (num) => num % 2 === 0;
-
 const getGameData = () => {
   const question = randomNum(1, 50);
   const result = (isEven(question) ? 'yes' : 'no');
   return [question, String(result)];
 };
 
+// Задание
 const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const startEvenGame = () => {
