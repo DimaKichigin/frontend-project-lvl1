8d8b3bc2b182414e758b0c1ea0038b374333251d
DimaKichigin <dimakichigin94@gmail.com>
2021-09-05 11:02:15 +0300

    mod src/games/gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -2,8 +2,8 @@ import randomNum from '../utils.js';
 import runEngine from '../game-engine.js';
 
 const rule = 'Find the greatest common divisor of given numbers.';
-const getGreatestCommonDivisor = (num1, num2) => (num2 === 0 ? num1 : getGreatestCommonDivisor(num2, num1 % num2));
-
+const getGreatestCommonDivisor = (num1, num2) => (num2 === 0
+  ? num1 : getGreatestCommonDivisor(num2, num1 % num2));
 const getGameData = () => {
   const num1 = randomNum(1, 50);
   const num2 = randomNum(1, 50);
